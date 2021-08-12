d1e51405768a14d14b9794939f396a9b72ca95b5
ccardinale98 <ccardinale98@gmail.com>
2021-08-12 14:49:58 -0400

    created psuedo code

---
+++ assets/js/script.js
@@ -0,0 +1,69 @@
+//hint: make one time block work and do the same thing for each one
+
+//create our document ready function() to make sure nothing runs before we load the page (all js goes inside this function)
+$(document).ready(function() {
+    //function() to listen for click events on the page
+        //grab the save event options 
+            //create variables to save user input and time
+        
+        //save values to local storage
+        //create message that says it was saved to local storage
+        //create some kind of timeOut value that removes the message after a few seconds
+
+    //create time updater function() 
+        //get current time (moment)
+
+        //create our loop to go over all the time blocks
+
+        //if else if statement to:
+            //check to see if we have passed our time
+                //hint: check out removeClass and addClass
+            //check to see if current hour(comming from moment) matches physical hour(coming from html)
+
+            //check to see if the time is in the future
+
+    //retrieve localStorage and render items to the correct time blocks(copy for all of the time blocks)
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
